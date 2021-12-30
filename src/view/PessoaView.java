ff7b3be07a959b72c362cd22ab1cdb6337b285f1
Sandro <sandrolp41@hotmail.com.br>
2021-12-30 17:33:24 -0300

    Ajusta View, Service, Repository #3

--- src/view/PessoaView.java
+++ src/view/PessoaView.java
@@ -10,76 +10,77 @@ import java.util.Scanner;
 
 public class PessoaView {
 
-	private Scanner scanner;
-	private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-	private PessoaService pessoaService;
+    private Scanner scanner;
+    private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private PessoaService pessoaService;
 
-	public PessoaView() {
-		this.scanner = new Scanner(System.in);
-		this.pessoaService = new PessoaService();
-	}
+    public PessoaView() {
+        this.scanner = new Scanner(System.in);
+        this.pessoaService = new PessoaService();
+    }
 
-	public void menuStart (){
-		int func = 1;
-		while (func != 0) {
+    public void menuStart() {
+        int func = 1;
+        while (func != 0) {
 
-			System.out.print("1 - Criar cadastro.\n2 - Mostrar todos cadastros.\n3 - Atualizar dado cadastrado.\n3 - Deletar Cadastro.\n4 - Encerrar aplicação.\n Digite a função que você deseja executar: ");
-			func = scanner.nextInt();
-			scanner.nextLine();
+            System.out.print("1 - Criar cadastro.\n2 - Mostrar todos cadastros.\n3 - Atualizar dado cadastrado.\n3 - Deletar Cadastro.\n4 - Encerrar aplicação.\n Digite a função que você deseja executar: ");
+            func = scanner.nextInt();
+            scanner.nextLine();
 
-			if (func == 1) {
-				cadastroPessoa ();
-			}
-			else if (func == 2) {
-			}
-			else if (func == 3) {
-				atualizarPessoa();
-			}
-			else if (func == 0) {
-				System.out.println("Encerrar aplicação");
-				scanner.close();
-			}
-			else {
-				System.out.println("Digito inválido\n");
-			}
-		}
-	}
+            if (func == 1) {
+                cadastrarPessoa();
+            } else if (func == 2) {
+            } else if (func == 3) {
+                atualizarPessoa();
+            } else if (func == 0) {
+                System.out.println("Encerrar aplicação");
+                scanner.close();
+            } else {
+                System.out.println("Digito inválido\n");
+            }
+        }
+    }
 
-	private void atualizarPessoa() {
-		PessoaModel pessoaNova = getPessoa();
+    private void atualizarPessoa() {
+        PessoaModel pessoaNova = getPessoa();
 
 //	TODO: chamar metódo responsável por atualizar
-	}
+    }
 
-	private void cadastroPessoa () {
-		PessoaModel pessoa = getPessoa();
-		pessoa.setDataCadastro(LocalDate.now());
+    private void cadastrarPessoa() {
+        PessoaModel pessoa = getPessoa();
+        pessoa.setDataCadastro(LocalDate.now());
 
-		System.out.println("Digite a nota final caso haja uma, ou somente aperte enter: ");
-		Double notaFinal = scanner.nextDouble();
+        String notaFinal = perguntarNota();
 
-		pessoaService.armazenarPessoa(pessoa, notaFinal);
-	}
+        pessoaService.armazenarDados(pessoa, notaFinal);
+    }
 
-	private PessoaModel getPessoa() {
-		PessoaModel pessoaModel = new PessoaModel();
+    private String perguntarNota() {
+        System.out.println("Digite a nota final caso haja uma, ou somente aperte enter: ");
+        return scanner.nextLine();
+    }
 
-		System.out.println("Digite o nome: ");
-		String nome = scanner.nextLine();
-		pessoaModel.setNome(nome);
+    private PessoaModel getPessoa() {
+        PessoaModel pessoaModel = new PessoaModel();
 
-		System.out.println("Digite o telefone: ");
-		String telefone = scanner.nextLine();
-		pessoaModel.setTelefone(telefone);
+        System.out.println("\nDigite o nome: ");
+        String nome = scanner.nextLine();
+        pessoaModel.setNome(nome);
 
-		System.out.println("Digite a data de nascimento: ");
-		String dataNascimentoS = scanner.nextLine();
-		LocalDate dataNascimento = LocalDate.parse(dataNascimentoS, FORMAT);
-		pessoaModel.setDataNascimento(dataNascimento);
+        System.out.println("Digite o telefone (modelo (51)33077964 ou (51)993077964): ");
+        String telefone = scanner.nextLine();
+        pessoaModel.setTelefone(telefone);
 
-		LocalDateTime ultimaAlteracao = LocalDateTime.now();
-		pessoaModel.setUltimaAlteracao(ultimaAlteracao);
+        System.out.println("Digite a data de nascimento (modelo 23/05/1995): ");
+        String nascimentoInput = scanner.nextLine();
 
-		return pessoaModel;
-	}
+        LocalDate dataNascimento = LocalDate.parse(nascimentoInput, FORMAT);
+        pessoaModel.setDataNascimento(dataNascimento);
+
+        LocalDateTime ultimaAlteracao = LocalDateTime.now();
+        pessoaModel.setUltimaAlteracao(ultimaAlteracao);
+
+        return pessoaModel;
+    }
 }
